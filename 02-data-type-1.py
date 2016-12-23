de941d46e66a67e3c6b4ef890d6542cae5b02297
Shanna SON <seung@MacBook-Air-de-Seung.local>
2016-12-24 02:01:11 +0900

    Add 02-data-type-1.py
    - Learn int, float, str

---
+++ 02-data-type-1.py
@@ -0,0 +1,25 @@
+# 정수 int - integer
+print(1 + 1)
+# 소수 float
+print(1.5 + 2.1)
+# 문자 str - string
+print("hello")
+
+# 문자열 더하기
+print("hello" + "1")
+
+# 정수 소수 간 변경
+# 나눈 후 소수점이 아닌 정수로 나타내는 2가지 방법
+print(int(4/2))
+print(4//2)
+# 소수로 나타내는 방법
+print(float(2))
+
+# 문자열 숫자 간 변경
+print("1")
+print(int("1"))
+print(int("1.5"))
+
+# 답 예상 - 1) 1 2) error
+# 정답 - error
+# 정수에 
